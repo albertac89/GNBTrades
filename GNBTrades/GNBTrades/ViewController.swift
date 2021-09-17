2a1e90887b9a0465720739371b629f9e871b5237
Aige Cortasa, Albert <Albert.Aige@gft.com>
2021-09-17 18:21:32 +0200

    generated project

---
+++ GNBTrades/GNBTrades/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GNBTrades
+//
+//  Created by Aige Cortasa, Albert on 17/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
